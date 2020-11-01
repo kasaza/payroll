05f6c5b91a2e7dda03fac2f15318876f3eade143
kasaza <kasazax@gmail.com>
2020-11-01 13:06:55 +0300

    Update sendmail.php

--- officer/sendmail.php
+++ officer/sendmail.php
@@ -37,8 +37,8 @@
 			$mail->SMTPAuth = true;                                   
 			$mail->Host = 'smtp.gmail.com'; 
 			$mail->SMTPAuth = true;                            
-			$mail->Username = 'jeffmwn@gmail.com';                 
-			$mail->Password = 'cathyjjjc';                         
+			$mail->Username = 'email@mail.com';                 
+			$mail->Password = '####';                         
 			$mail->SMTPSecure = 'tls';                           
 			$mail->Port = 587;                                  
 			$mail->setFrom('jeffmwn@gmail.com', 'E-Payroll TUM');
@@ -145,8 +145,8 @@
 			$mail->SMTPAuth = true;                                   
 			$mail->Host = 'smtp.gmail.com'; 
 			$mail->SMTPAuth = true;                            
-			$mail->Username = 'jeffmwn@gmail.com';                 
-			$mail->Password = 'cathyjjjc';                         
+			$mail->Username = 'email@mail.com';                 
+			$mail->Password = '#####';                         
 			$mail->SMTPSecure = 'tls';                           
 			$mail->Port = 587;                                  
 			$mail->setFrom('jeffmwn@gmail.com', 'E-Payroll');
